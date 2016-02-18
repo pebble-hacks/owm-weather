13234d1e7a5a4262442fd1008db08ea7f709a36e
Cat Haines <cat@pebble.com>
2016-02-18 12:27:27 -0800

    Dynamic app keys

--- test-app/src/js/app.js
+++ test-app/src/js/app.js
@@ -1,6 +1,6 @@
-var OWMWeather = require('lib/owm_weather.js')
+var OWMWeather = require('lib/owm_weather.js');
 
-var owmWeather = new OWMWeather();
+var owmWeather = new OWMWeather({ baseAppKey: 10 });
 
 Pebble.addEventListener('ready', function(e) {
   console.log('PebbleKit JS ready!');
--- test-app/src/js/lib/owm_weather.js
+++ test-app/src/js/lib/owm_weather.js
@@ -1,6 +1,35 @@
-var OWMWeather = function() {
-
+var OWMWeather = function(options) {
   this._apiKey = '';
+
+  options = options || {};
+  this._appKeyBase = options.baseAppKey || 0;
+
+  this._appKeys = {
+    "Request": 0,
+    "Reply": 1,
+    "Description": 2,
+    "DescriptionShort": 3,
+    "Name": 4,
+    "TempK": 5,
+    "Pressure": 6,
+    "WindSpeed": 7,
+    "WindDirection": 8,
+    "BadKey": 9,
+    "LocationUnavailable": 10
+  };
+
+  this.getAppKey = function(keyName) {
+    return this._appKeyBase + this._appKeys[keyName];
+  };
+
+  this.sendAppMessage = function(obj, onSuccess, onError) {
+    var msg = {};
+    for(var key in obj) {
+      msg[this.getAppKey(key)] = obj[key];
+    }
+
+    Pebble.sendAppMessage(msg, onSuccess, onError);
+  };
 
   this._xhrWrapper = function(url, type, callback) {
     var xhr = new XMLHttpRequest();
@@ -9,20 +38,20 @@ var OWMWeather = function() {
     };
     xhr.open(type, url);
     xhr.send();
-  }
+  };
 
   this.sendToPebble = function(json) {
-    Pebble.sendAppMessage({
-      'OWMWeatherAppMessageKeyReply': 1,
-      'OWMWeatherAppMessageKeyDescription': json.weather[0].description,
-      'OWMWeatherAppMessageKeyDescriptionShort': json.weather[0].main,
-      'OWMWeatherAppMessageKeyTempK': Math.round(json.main.temp),
-      'OWMWeatherAppMessageKeyName': json.name,
-      'OWMWeatherAppMessageKeyPressure': Math.round(json.main.pressure),
-      'OWMWeatherAppMessageKeyWindSpeed': Math.round(json.wind.speed),
-      'OWMWeatherAppMessageKeyWindDirection': Math.round(json.wind.deg)
+    this.sendAppMessage({
+      'Reply': 1,
+      'Description': json.weather[0].description,
+      'DescriptionShort': json.weather[0].main,
+      'TempK': Math.round(json.main.temp),
+      'Name': json.name,
+      'Pressure': Math.round(json.main.pressure),
+      'WindSpeed': Math.round(json.wind.speed),
+      'WindDirection': Math.round(json.wind.deg)
     });
-  }
+  };
 
   this._onLocationSuccess = function(pos) {
     var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
@@ -35,21 +64,21 @@ var OWMWeather = function() {
         this.sendToPebble(JSON.parse(responseText));
       } else {
         console.log('owm-weather: API response was bad. Wrong API key?');
-        Pebble.sendAppMessage({ 'OWMWeatherAppMessageKeyBadKey': 1 });
+        this.sendAppMessage({ 'BadKey': 1 });
       }
     }.bind(this));
-  }
+  };
 
   this._onLocationError = function(err) {
     console.log('owm-weather: Location error');
-    Pebble.sendAppMessage({
-      'OWMWeatherAppMessageKeyLocationUnavailable': 1
+    this.sendAppMessage({
+      'LocationUnavailable': 1
     });
-  }
+  };
 
   this.appMessageHandler = function(dict) {
-    if(dict.payload['OWMWeatherAppMessageKeyRequest']) {
-      this._apiKey = dict.payload['OWMWeatherAppMessageKeyRequest'];
+    if(dict.payload[this.getAppKey('Request')]) {
+      this._apiKey = dict.payload[this.getAppKey('Request')];
       console.log('owm-weather: Got fetch request from C app');
 
       navigator.geolocation.getCurrentPosition(
@@ -59,9 +88,7 @@ var OWMWeather = function() {
           maximumAge: 60000
       });
     }
-
-    return true;
   };
-}
+};
 
 module.exports = OWMWeather;
